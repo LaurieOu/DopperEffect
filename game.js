e6057d5b6f372cad7a2409fa51e040fb0dbf7d16
laurieou <laurieou@gmail.com>
2016-04-15 15:23:23 -0700

    add planet and working scroll bar

--- game.js
+++ game.js
@@ -5,8 +5,29 @@ var alpha=0;
 
 function showValue(newValue)
 {
-	document.getElementById("range").innerHTML=newValue;
+	document.getElementById("range").value=newValue;
 };
+
+function updateValue(val) {
+  if (val >= -100 && val <= 100) {
+    if(event.keyCode == 13) {
+      document.getElementById("scrollbar").value=val;
+    }
+  } else {
+    try {
+      throw "Number must be between -100 and 100!"
+    }
+    catch(err) {
+      message.innerHTML = err;
+      setTimeout(fade_out, 500);
+    }
+  }
+
+};
+
+function fade_out() {
+  $("#message").fadeOut().empty();
+}
 
 function changeColor(val,color){
   var m = document.getElementById("mydiv");
@@ -18,4 +39,6 @@ function changeColor(val,color){
     }
 
     m.style.backgroundColor='rgba('+red+','+green+','+blue+','+alpha+')';
+    m.style.height=val;
+    m.style.width=val * 2;
 };

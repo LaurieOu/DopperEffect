72508c33e75824a24e65d050125053c97df7d666
laurieou <laurieou@gmail.com>
2016-04-15 20:33:33 -0700

    star gets bigger and smaller

--- game.js
+++ game.js
@@ -25,12 +25,18 @@ function updateValue(val) {
 function changeStarColor(val) {
   if(val == 0) {
     star.style.backgroundColor = 'rgba(0,0,0,0)';
+    star.style.width = 300;
+    star.style.height = 300;
   } else if (val > 0) {
     val = val * 2;
-    star.style.backgroundColor = 'rgba(0,0,'+val+',0.8)';
+    star.style.backgroundColor = 'rgba(0,0,'+val+',0.3)';
+    star.style.width = val + 300;
+    star.style.height = val + 300;
   } else {
     val = val * -2;
-    star.style.backgroundColor = 'rgba('+val+',0,0,0.8)';
+    star.style.width = val * -1 + 300;
+    star.style.height = val * -1 + 300;
+    star.style.backgroundColor = 'rgba('+val+',0,0,0.3)';
   }
 }
 
